1ba4c37d0eec49d8f2ffc87280d6704ed511ba57
neetmangat <neetmangat@gmail.com>
2021-01-14 11:59:01 -0600

    testing start page mobile tweaks

--- src/pages/start.js
+++ src/pages/start.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import start from '../images/start.jpg'
 import './start.css';
 
 export default class Start extends Component {
     render() {
         return(
-            <div className="row justify-content-center">
-                <div className="startpage" onClick={this.props.onClick}>
+            <div className="h-100 d-flex justify-content-center align-items-center">
+                <div className="text-center">
+                    <img src={start} className="startimage" onClick={this.props.onClick} />
                     <p className="starttext blink_me textspaced" onClick={this.props.onClick}>START GAME</p>
                 </div>
             </div>
