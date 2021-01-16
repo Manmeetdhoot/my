e1714d12cc29d21bdd037634c832c94be66e8113
neetmangat <neetmangat@gmail.com>
2021-01-16 03:31:29 -0600

    added music clip

--- src/pages/chill.js
+++ src/pages/chill.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './chill.css';
+import song from '../sounds/song2.m4a';
+
 
 export default class Chill extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            play: false,
+        }
         this.handleClick = this.handleClick.bind(this);
     }
 
@@ -13,18 +18,29 @@ export default class Chill extends Component {
         this.props.onClick(page);
     }
 
+
+
     render() {
+        var audio = new Audio(song)
+
         return(
-            <div className="mt-5">
-                <div className="row justify-content-center mb-3">
-                    <button className="btn-site pl-5 pr-5 textspaced btn-larger" id="MENU" onClick={this.handleClick}>
+            <div>
+                <div className="row justify-content-center mt-5">
+                    <button className="btn-site pl-5 pr-5 mr-3 textspaced btn-larger" id="MENU" onClick={this.handleClick}>
                         MENU
                     </button>
-                </div>
-                <div className="row justify-content-center">
-                    <h4 className="textyellow textspaced">Sound On Please</h4>
+                    <button className="btn-site ml-3 textspaced btn-larger" onClick={ () => audio.play() }>
+                        PLAY MUSIC
+                    </button>
                 </div>
             </div>
         )
     }
 }
+
+/*
+<div className="text-center textyellow textspaced">
+    <h5>Song: "Midas Touch"</h5> 
+    <h5>Artist: "Shammy"</h5>
+</div>
+*/
